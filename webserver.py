873af95a201183dee5da5d942b72aacd82e97837
TrekWest <jayw201703@gmail.com>
2026-03-06 19:44:28 +1100

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -5,7 +5,7 @@ app = Flask('')
 def home():
     return "Discord Bot"
 def run():
-    app.run(host="0.0.0.0", port = 8080)
+    app.run(host="0.0.0.0", port = 10000)
 def keep_alive():
     t = Thread(target=run)
     t.start()
